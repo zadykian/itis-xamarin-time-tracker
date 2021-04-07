6e79af28f59fac6e3cbd3c82209fb432745d017b
Alexander Zadykian <zadykian@outlook.com>
2021-04-07 23:51:23 +0300

    *

--- TimeTrackerTutorial.Android/MainActivity.cs
+++ TimeTrackerTutorial.Android/MainActivity.cs
@@ -14,8 +14,9 @@ namespace TimeTrackerTutorial.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            // todo
+            // TabLayoutResource = Resource.Layout.Tabbar;
+            // ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
 
