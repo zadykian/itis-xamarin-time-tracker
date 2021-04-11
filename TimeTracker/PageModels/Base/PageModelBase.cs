616f517886c17e73f964e59bfd2ecd30cbbe1a59
Alexander Zadykian <zadykian@outlook.com>
2021-04-11 18:43:52 +0300

    *

--- TimeTracker/PageModels/Base/PageModelBase.cs
+++ TimeTracker/PageModels/Base/PageModelBase.cs
@@ -4,35 +4,9 @@ namespace TimeTracker.PageModels.Base
 {
     internal class PageModelBase : ExtendedBindableObject
     {
-        string _title;
-        /// <summary>
-        /// Title of the Page, settable in the PageModel
-        /// </summary>
-        public string Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value);
-        }
-
-        bool _isLoading;
-        /// <summary>
-        /// Flag to notify the Page on network activity
-        /// </summary>
-        public bool IsLoading
-        {
-            get => _isLoading;
-            set => SetProperty(ref _isLoading, value);
-        }
-
-
         /// <summary>
         /// Performs Page Model initialization Asynchronously
         /// </summary>
-        /// <param name="navigationData"></param>
-        /// <returns></returns>
-        public virtual Task InitializeAsync(object navigationData)
-        {
-            return Task.CompletedTask;
-        }
+        public virtual Task InitializeAsync(object navigationData) => Task.CompletedTask;
     }
 }
