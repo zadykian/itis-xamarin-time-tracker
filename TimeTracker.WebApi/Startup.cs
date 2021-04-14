500e02fa3971bb2334d3572253b315d5c2925e58
Alexander Zadykian <zadykian@outlook.com>
2021-04-14 23:37:09 +0300

    Startup

--- TimeTracker.WebApi/Startup.cs
+++ TimeTracker.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using TimeTracker.Services.Account;
+using TimeTracker.Services.Images;
 using TimeTracker.Services.TimeTracking;
 
 namespace TimeTracker.WebApi
@@ -12,6 +13,7 @@ namespace TimeTracker.WebApi
 			=> services
 				.AddSingleton<IAccountService>(_ => new SqliteAccountService(Environment.CurrentDirectory))
 				.AddSingleton<ITrackedPeriodService>(_ => new SqliteTrackedPeriodService(Environment.CurrentDirectory))
+				.AddSingleton<IImageService>(_ => new SqliteImageService(Environment.CurrentDirectory))
 				.AddControllers();
 
 		public void Configure(IApplicationBuilder app) => app.UseRouting();
