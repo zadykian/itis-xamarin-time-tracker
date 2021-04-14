b19187e1b0cdc462c54cfc70e077b226edb1b2dc
zadykian <zadykian@outlook.com>
2021-04-14 18:05:00 +0300

    renamed project

--- TimeTracker.App.Android/Properties/AssemblyInfo.cs
+++ TimeTracker.App.Android/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeTracker.Android")]
+[assembly: AssemblyTitle("TimeTracker.App.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TimeTracker.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("TimeTracker.App.Android")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
